891dc2b84c771474b65bec20b1569bb1dcbd6e46
WujecDamian <wujecdamian12@gmail.com>
2026-02-28 19:13:31 +0100

    feat(index.js): add import from task.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 //put inside src/index.js
 import './styles.css'
+import { createTask, logTask } from './scripts/data/task.js'
+
+/* test tasks.js */
+tasks.push(createTask('projecttest', 'titletest', 'desctst', 'dueTest'))
+logTask()
