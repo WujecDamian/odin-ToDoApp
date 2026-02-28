529f2eade8225d7c640cbdbd2fbe6ab1c2e7a5f4
WujecDamian <wujecdamian12@gmail.com>
2026-02-28 15:17:56 +0100

    feat(task.js): add create task function and tasks array

--- src/scripts/data/task.js
+++ src/scripts/data/task.js
@@ -1,1 +1,15 @@
 /* everything related to task [CRUD] */
+
+/* create task object factory  */
+export let tasks = []
+
+export function createTask (projectName, title, description, dueDate) {
+  return {
+    projectName: projectName,
+    title: title,
+    description: description,
+    dueDate: dueDate,
+    creationDate: '/* function from date.js */',
+    notes: '' //User will be able to add notes (for now empty)
+  }
+}
