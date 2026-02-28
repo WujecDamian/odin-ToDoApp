f3425656853b3bd9ee98a045769f1cabca3f6258
WujecDamian <wujecdamian12@gmail.com>
2026-02-28 20:08:32 +0100

    feat: add localstorage function imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 //put inside src/index.js
 import './style.css'
-import { createTask, getTasks } from './scripts/data/task.js'
+import { createTask } from './scripts/data/task.js'
+import { createProject } from './scripts/data/project.js'
+import {
+  getProjectsFromLocalStorage,
+  getTasksFromLocalStorage
+} from './scripts/data/storage.js'
+
 addEventListener('DOMContentLoaded', event => {
   createTask('projecttest', 'titletest', 'desctst', 'dueTest')
+  createTask('xx', 'dd', 'ss', 'ew')
+
   console.log(getTasks())
 })
