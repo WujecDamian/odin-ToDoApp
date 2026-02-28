84ec70a5ed5f5ed35fc050cbc89a1ee230708e77
WujecDamian <wujecdamian12@gmail.com>
2026-02-28 20:07:20 +0100

    feat(storage.js): add getter functions

--- src/scripts/data/storage.js
+++ src/scripts/data/storage.js
@@ -4,7 +4,18 @@
 export function saveProjectLocalStorage (projects) {
   localStorage.setItem('projects', JSON.stringify(projects))
 }
+/* get projects */
+export function getProjectsFromLocalStorage () {
+  return JSON.parse(localStorage.getItem('projects'))
+}
+
+/* _________________________________________________________________ */
+
 /* function to save tasks to localstorage */
 export function saveTaskLocalStorage (tasks) {
   localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+/* get tasks */
+export function getTasksFromLocalStorage () {
+  return JSON.parse(localStorage.getItem('tasks'))
 }
