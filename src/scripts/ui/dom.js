de5a3e589da7b299b928b6392511e6d420ecb150
WujecDamian <wujecdamian12@gmail.com>
2026-02-28 21:51:45 +0100

    feat: add button queryselectors and listeners

--- src/scripts/ui/dom.js
+++ src/scripts/ui/dom.js
@@ -1,1 +1,6 @@
+import { openProjectModal, closeProjectModal } from './ui.js'
 /* this module gets elements (mostly querySelectors) */
+const addTaskBtn = document.querySelector('#button-task')
+
+const addProjectBtn = document.querySelector('#button-project')
+addProjectBtn.addEventListener('click', openProjectModal)
