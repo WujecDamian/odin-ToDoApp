0a4143d45aded367745400115a5959e16627ba49
WujecDamian <wujecdamian12@gmail.com>
2026-02-28 21:10:15 +0100

    feat(ui): add functions to open and close project modal

--- src/scripts/ui/ui.js
+++ src/scripts/ui/ui.js
@@ -1,1 +1,17 @@
 /* this module puts dom elements on screen */
+import { projectModal } from './ui/projectModal'
+/* )      )      )      )      )   what I need?> */
+/* modal (dialog - .showModal(), close()) to create projects */
+export function openProjectModal () {
+  projectModal.showModal()
+}
+export function closeProjectModal () {
+  projectModal.close()
+}
+/* modal to create tasks */
+/* function to render projects */
+/* function to render tasks */
+/* function to delete projects */
+/* function to delete tasks */
+/* function to check that task is completed */
+/* function to add note to task (it opens box and you can modify there or just read (so no need to add read note function)*/
