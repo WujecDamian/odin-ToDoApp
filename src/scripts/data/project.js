f294b7decd0465dc5caeedfe29db1c39a4528636
WujecDamian <wujecdamian12@gmail.com>
2026-03-01 01:12:01 +0100

    feat: add loadProject function

--- src/scripts/data/project.js
+++ src/scripts/data/project.js
@@ -1,9 +1,20 @@
 /* everything related to project [CRUD] */
-import { saveProjectLocalStorage } from './storage.js'
-
-/* create project object factory  */
-let projects = []
-
+import {
+  saveProjectLocalStorage,
+  getProjectsFromLocalStorage
+} from './storage.js'
+export function loadProject () {
+  /* create project object factory  */
+  let projects = getProjectsFromLocalStorage() || []
+  console.log(projects)
+  if (projects.length === 0) {
+    const project = {
+      id: crypto.randomUUID(),
+      projectName: 'General'
+    }
+    saveProjectLocalStorage(project) // ← tu zapisujesz
+  }
+}
 export function createProject (projectName) {
   const project = {
     id: crypto.randomUUID(),
