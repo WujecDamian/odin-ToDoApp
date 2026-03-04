def52fc544a93b89e1243eea14e28d85ef424cd1
WujecDamian <wujecdamian12@gmail.com>
2026-03-04 01:30:57 +0100

    feat: add delete icons

--- src/scripts/ui/ui/renderProjects.js
+++ src/scripts/ui/ui/renderProjects.js
@@ -2,9 +2,12 @@ import {
   getProjectsFromLocalStorage,
   saveProjectLocalStorage
 } from '../../data/storage.js'
+import deleteSvg from '../../../assets/delete.svg'
 import { deleteProject } from './deleteProject.js'
 import { setActiveProject } from './setActiveProject.js'
 import { createProject } from '../../data/project.js'
+export const currentProjectDom = document.querySelector('.current__project')
+
 export function renderProjects () {
   const projectsList = document.querySelector('.projects__list')
   let projects = getProjectsFromLocalStorage() || []
@@ -20,15 +23,17 @@ export function renderProjects () {
     const projectText = document.createElement('span')
     projectText.innerText = element.projectName
     projectText.className = 'project__text'
-    const projectDelete = document.createElement('input')
-    projectDelete.type = 'button'
+    const projectDelete = document.createElement('button')
     projectDelete.className = 'project__delete'
-    projectDelete.value = 'Delete'
+    const deleteIcon = document.createElement('img')
+    deleteIcon.src = deleteSvg
+    projectDelete.append(deleteIcon)
     projectDelete.addEventListener('click', deleteProject)
     projectEl.append(projectText)
     projectEl.append(projectDelete)
     projectsList.appendChild(projectEl)
     let activeProject = projects[0].projectName
+    currentProjectDom.innerText = activeProject
 
     projectEl.addEventListener('click', setActiveProject)
   })
--- src/scripts/ui/ui/setActiveProject.js
+++ src/scripts/ui/ui/setActiveProject.js
@@ -1,7 +1,9 @@
 import { getProjectsFromLocalStorage } from '../../data/storage.js'
 import { renderTasks } from './renderTasks.js'
+import { currentProjectDom } from './renderProjects.js'
 export function setActiveProject (e) {
   let projects = getProjectsFromLocalStorage()
   let activeProject = e.target.innerText
   renderTasks(activeProject)
+  currentProjectDom.innerText = activeProject
 }
