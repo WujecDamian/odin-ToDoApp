6b02e7a1b7a4990a93fe3597d336a3f7168d20ca
WujecDamian <wujecdamian12@gmail.com>
2026-03-07 21:17:53 +0100

    chore: add addNote module and write pseudo pseudocode for it

---
+++ src/scripts/ui/ui/addNote.js
@@ -0,0 +1,6 @@
+//code add note function.
+
+//how it should work?
+//click on button
+//modal shows up with textarea and add button
+//on task appears arrow that you can click and expand to see a note.
